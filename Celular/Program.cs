b18b22598c63629e36876f5499bcc313db766cde
Kleiton Lucas <kleitonlucs@gmail.com>
2024-05-13 12:27:53 -0300

    Finalizando o desafio

---
+++ Celular/Program.cs
@@ -0,0 +1,13 @@
+﻿using Celular.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "99997777", modelo: "NK X.2", imei: "1234567890", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("App Teste");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "98883333", modelo: "I.5", imei: "0987654321", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("App Teste");
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DIO_celular
+Desafio de projeto do curso .NET Developer da DIO
+
+## Descrição
+Um projeto simples para fins de estudo
+
+## Execução
+Não tem comunicação com outros sistemas, para executar só precisa abrir o projeto e dar o comando para executar.
+
+```dotnet run```
